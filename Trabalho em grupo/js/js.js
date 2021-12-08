e88144eaf2489b0da27f4b067021aea18e1218c3
Gabriel Rosa <gabriel.rosa15@outlook.com>
2021-12-08 16:47:14 -0300

    Trabalho Senai
    Trabalho feito no Senai

---
+++ Trabalho em grupo/js/js.js
@@ -0,0 +1,14 @@
+function button2(){
+    document.getElementById("button2").innerHTML = "Você não resistou jovem."
+    document.getElementById("button2").style.fontWeight = "italic"
+    document.getElementById("button2").style.fontSize = "25px"
+    document.getElementById("colocaapalavra").innerHTML = "Era uma vez um jovem personagem, no qual viu um botão em que foi jogado magia, o jovem não reistiu ao botão e a sua magia e acabou clicando no mesmo isto fez com que ele modificasse o site que regia o universo, então como o site foi modificado o univeso também se modificou."
+    document.getElementById("colocaapalavra").fontWeight = "bold"
+}
+function button3(){
+    document.getElementById("button3").innerHTML = "Roberto de Roberto não Roberto de aberto"
+    document.getElementById("button3").style.color = "red"
+}
+function button4(){
+    document.getElementById("imagem").scr =Imagem/unnamed.jpg 
+}
